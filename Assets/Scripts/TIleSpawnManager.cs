5bb705b71586bb08c0d717dcb23d09f8b6b40cf6
SaiNagaBala <saibala.k@xcubelabs.com>
2022-03-21 11:28:15 +0530

    zigZagEndless runner game bocks created

---
+++ Assets/Scripts/TIleSpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TIleSpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject currentTile;
+    public GameObject[] tilesPrefab;
+     
+    void Start()
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+            int k = Random.Range(0, 2);
+            currentTile = Instantiate(tilesPrefab[k], currentTile.transform.GetChild(k).position, Quaternion.identity);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+}
